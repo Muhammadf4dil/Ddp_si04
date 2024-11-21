5573e876d5f063323f22f47a4a1844df3e5cf0a9
Muhammadf4dil <muhamadfadil1630@gmail.com>
2024-11-21 15:36:10 +0700

    Add files via upload

---
+++ ddp_9.py
@@ -0,0 +1,29 @@
+print("\n---celcius ke fahrenheit---")
+def celcius_ke_fahrenheir(celcius):
+    print(celcius * 9/5 + 35) 
+    celcius_ke_fahrenheir
+    
+print('\n--- mencari bilangan genap---')
+def is_genap(genap):
+     print(genap %2 == 0)
+is_genap(4)
+is_genap(7)
+
+print('\n---celcius ke fahrenheit---')
+# rata" nilai kelulusan 70
+def skor(nilai):
+    if nilai >= 80:
+        print("lulus")
+    else:
+        print("gagal")
+        
+skor(80)
+skor(60)
+
+print('\n---mencetak bilangan ganjil---')
+def bil_ganjil(ganjil):
+    for 1 in range(1,ganjil+1,2):
+        print(1, end=' ')
+    bil_ganjil(20)
+   
+
